c5bc3ef0acd7322964a1d799a778831bf1e54e5d
Benjamin Eckel <bhelx@simst.im>
2022-12-20 15:32:33 -0600

    format

--- config/dev.exs
+++ config/dev.exs
@@ -28,9 +28,9 @@ config :game_box, GameBoxWeb.Endpoint,
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
     esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
     sass: {
-        DartSass,
-        :install_and_run,
-        [:default, ~w(--embed-source-map --source-map-urls=absolute --watch)]
+      DartSass,
+      :install_and_run,
+      [:default, ~w(--embed-source-map --source-map-urls=absolute --watch)]
     }
   ]
 
@@ -78,7 +78,6 @@ config :phoenix, :stacktrace_depth, 20
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
-
 
 if File.exists?(Path.join([__DIR__, "dev.secret.exs"])) do
   import_config "dev.secret.exs"
