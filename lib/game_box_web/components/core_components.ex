3db01b8cb34e7d0c1039c890e16997a423fb17b6
Amy Murphy <amymurphy114@gmail.com>
2023-02-13 09:28:02 -0600

    refactor: Update core component style

--- lib/game_box_web/components/core_components.ex
+++ lib/game_box_web/components/core_components.ex
@@ -203,7 +203,7 @@ defmodule GameBoxWeb.CoreComponents do
   def simple_form(assigns) do
     ~H"""
     <.form :let={f} for={@for} as={@as} {@rest}>
-      <div class="space-y-8 bg-white mt-10">
+      <div class="space-y-8 mt-10">
         <%= render_slot(@inner_block, f) %>
         <div :for={action <- @actions} class="mt-2 flex items-center justify-between gap-6">
           <%= render_slot(action, f) %>
