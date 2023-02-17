0cfb621c37a259afec29442b3ff5672035a7d845
Amy Murphy <amymurphy114@gmail.com>
2023-02-17 14:13:33 -0600

    fix: Adjust code component for responsive design

--- lib/game_box_web/components/code.ex
+++ lib/game_box_web/components/code.ex
@@ -8,8 +8,8 @@ defmodule GameBoxWeb.Code do
 
   def code(assigns) do
     ~H"""
-    <code>
-      <pre class="p-5 text-white bg-black border-l border-l-3 border-zinc-300"><%= HtmlEntities.decode(@content) %></pre>
+    <code class="max-w-full">
+      <pre class="p-5 text-white bg-black border-l border-l-3 border-zinc-300 !max-w-full  overflow-scroll"><%= HtmlEntities.decode(@content) %></pre>
     </code>
     """
   end
