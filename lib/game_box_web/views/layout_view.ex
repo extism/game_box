799dc55117314b4987181c1be76381c56c89766e
Amy Murphy <amymurphy114@gmail.com>
2023-02-24 08:27:27 -0600

    fix: Github Actions warnings

--- lib/game_box_web/views/layout_view.ex
+++ lib/game_box_web/views/layout_view.ex
@@ -1,6 +1,5 @@
 defmodule GameBoxWeb.LayoutView do
   use GameBoxWeb, :view
-  import Phoenix.LiveView.JS
 
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
