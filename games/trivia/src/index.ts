0ee021136499b779620fe88f24816f82b7f5ae7a
Benjamin Eckel <bhelx@simst.im>
2023-02-24 17:16:06 -0600

    remove console.logs

--- games/trivia/src/index.ts
+++ games/trivia/src/index.ts
@@ -19,20 +19,13 @@ interface GameConstraints {
 
 export function get_constraints(): number {
   const constraints: GameConstraints = { min_players: 2, max_players: 8 }
-  console.log("get constraints")
-  console.log(JSON.stringify(constraints))
   outputJson(constraints)
   return 0
 }
 
 export function render(): number {
   const assigns: Assigns = inputJson()
-  console.log("Render: ")
-  console.log(JSON.stringify(assigns))
   const game = new Game(getState())
-  //const game = new Game({players: ["ben", "steve"], state: {name: "prompting", questionIndex: 0, answers: {"steve": 0}}})
-  console.log(JSON.stringify(game))
-  //const game = new Game(["steve", "ben"])
   outputString(game.render(assigns))
   return 0
 }
@@ -40,16 +33,10 @@ export function render(): number {
 export function handle_event(): number {
   const event: LiveEvent = inputJson()
   const game = new Game(getState())
-  //const game = new Game({players: ["ben", "steve"]})
-  console.log("event: ")
-  console.log(JSON.stringify(event))
-  console.log(JSON.stringify(game))
 
   const assigns = game.handleEvent(event)
   assigns.version = game.version
-  console.log(JSON.stringify(assigns))
 
-  console.log(JSON.stringify(game))
   outputJson(assigns)
   setState(game)
 
