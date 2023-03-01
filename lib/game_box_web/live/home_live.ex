0382ea31d459dfb316464054ad9df5fd817316b3
Amy Murphy <amymurphy114@gmail.com>
2023-03-01 11:02:02 -0600

    refactor: Adjust homepage layout

--- lib/game_box_web/live/home_live.ex
+++ lib/game_box_web/live/home_live.ex
@@ -5,15 +5,17 @@ defmodule GameBoxWeb.HomeLive do
   def render(assigns) do
     ~H"""
     <.hero class="pt-0 md:pt-16 !mb-0" header="Start or join an arena" />
-    <div class="w-full flex justify-center my-24">
-      <div class="w-1/2 md:w-1/4 border-r border-zinc-700 pr-6">
+    <div class="w-full flex justify-center my-16">
+      <div class="w-1/2 md:w-1/4 border-r border-zinc-700 pr-6 py-12">
         <.link navigate={~p"/start"}>
           <.button class="w-full" label="Start" />
+          <.p class="text-center text-sm">Host an arena and invite your friends to join!git</.p>
         </.link>
       </div>
-      <div class="w-1/2 md:w-1/4 pl-6">
+      <div class="w-1/2 md:w-1/4 pl-6 py-12">
         <.link navigate={~p"/join"}>
           <.button class="w-full" label="Join" />
+          <.p class="text-center text-sm">If you already have an arena code, use it here!</.p>
         </.link>
       </div>
     </div>
