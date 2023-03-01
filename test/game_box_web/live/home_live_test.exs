308af3ca782462e38c9cd949787e65d8bb5d9eb0
Nick Schello <nick@revelry.co>
2023-03-01 18:06:57 -0500

    fix: re-add test file

---
+++ test/game_box_web/live/home_live_test.exs
@@ -0,0 +1,20 @@
+defmodule GameBoxWeb.HomeLiveTest do
+  use GameBoxWeb.ConnCase
+
+  describe "home" do
+    test "join an arena", ctx do
+      %{conn: conn} = ctx
+      {:ok, view, _html} = live(conn, Routes.live_path(GameBoxWeb.Endpoint, GameBoxWeb.HomeLive))
+      player_id = get_session(conn, :player_id)
+
+      view
+      |> element("#join_arena")
+      |> render_submit(%{arena_form: %{player_name: "Joe", arena_id: "abcd"}})
+
+      path = ~p"/arena/abcd"
+      assert {^path, %{}} = assert_redirect(view)
+      assert %{id: ^player_id, name: "JOE"} = GameBox.Players.get_player("ABCD", player_id)
+      assert %{arena_id: "ABCD"} = GameBox.Arena.state("ABCD")
+    end
+  end
+end
