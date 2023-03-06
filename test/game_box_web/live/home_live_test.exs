d5372bb8f5dc119b5b930d46927c2728c1a5180e
Nick Schello <nick@revelry.co>
2023-03-06 13:21:16 -0500

    fix: start arena before joining

--- test/game_box_web/live/home_live_test.exs
+++ test/game_box_web/live/home_live_test.exs
@@ -1,20 +1,23 @@
 defmodule GameBoxWeb.HomeLiveTest do
   use GameBoxWeb.ConnCase
+  alias GameBox.Arena
 
   describe "home" do
     test "join an arena", ctx do
       %{conn: conn} = ctx
       {:ok, view, _html} = live(conn, Routes.live_path(GameBoxWeb.Endpoint, GameBoxWeb.HomeLive))
       player_id = get_session(conn, :player_id)
+      arena_id = "abcd"
+      Arena.start(arena_id)
 
       view
       |> element("#join_arena")
-      |> render_submit(%{arena_form: %{player_name: "Joe", arena_id: "abcd"}})
+      |> render_submit(%{arena_form: %{player_name: "Joe", arena_id: arena_id}})
 
       path = ~p"/arena/abcd"
       assert {^path, %{}} = assert_redirect(view)
-      assert %{id: ^player_id, name: "JOE"} = GameBox.Players.get_player("ABCD", player_id)
-      assert %{arena_id: "ABCD"} = GameBox.Arena.state("ABCD")
+      assert %{id: ^player_id, name: "JOE"} = GameBox.Players.get_player(arena_id, player_id)
+      assert %{arena_id: ^arena_id} = GameBox.Arena.state(arena_id)
     end
   end
 end
