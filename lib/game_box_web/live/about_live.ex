4c6a4b3659584d1a86ac1ed6d4652a1fc021c6b9
Amy Murphy <amymurphy114@gmail.com>
2023-03-10 13:18:56 -0600

    feat: Add About Page

--- lib/game_box_web/live/about_live.ex
+++ lib/game_box_web/live/about_live.ex
@@ -1,14 +1,52 @@
 defmodule GameBoxWeb.AboutLive do
   use GameBoxWeb, :live_view
 
+  alias GameBox.Games
+
+  def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:games, Games.list_games())
+
+    {:ok, socket}
+  end
+
   def render(assigns) do
     ~H"""
-    <.hero header="About Gamebox" />
-    <div class="flex flex-col items-center justify-center text-center">
-      <.p>
-        Some About Info
-      </.p>
-      <.link class="text-primary text-xl" href={~p"/auth/github"}>Sign in with Github</.link>
+    <.hero
+      header="About Gamebox"
+      subtext="Gamebox is lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ut arcu gravida mauris hendrerit vehicula at nec lacus. Maecenas sed dolor eget nisl commodo condimentum luctus vitae risus.
+    "
+    />
+
+    <div class="flex flex-col md:flex-row">
+      <div class="w-full text-center">
+        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-6 md:gap-x-12 md:gap-y-12 mb-12">
+          <%= for game <- @games do %>
+            <.card>
+              <.card_media :if={game.artwork} src={game.artwork} />
+              <.card_media
+                :if={!game.artwork}
+                src="/images/donut.png"
+                class="flex justify-center w-48 p-6"
+              />
+              <.card_content
+                author={"@#{game.user.gh_login}"}
+                author_link={"https://github.com/#{game.user.gh_login}"}
+                heading={game.title}
+              />
+              <.card_footer>
+                <.button
+                  phx-click="select_game"
+                  phx-value-game_id={game.id}
+                  label="View Details"
+                  class="w-full"
+                />
+              </.card_footer>
+            </.card>
+          <% end %>
+        </div>
+      </div>
     </div>
     """
   end
